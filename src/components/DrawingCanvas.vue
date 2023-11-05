e9e7356d361784572d0b0ed6d5a2983d742b424c
Dustin Schipper <schippde@gmail.com>
2023-11-05 11:34:43 -0600

    about to try installing prettier

---
+++ src/components/DrawingCanvas.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>Drawing Canvas</div>
+</template>
+
+<script setup>
+console.log('drawing canvas');
+</script>
--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,9 +1,7 @@
 <template>
-  <v-app-bar flat>
+  <v-app-bar flat height="40" color="grey-lighten-2">
     <v-app-bar-title>
-      <v-icon icon="mdi-circle-slice-6" />
-
-      Essentials Preset
+      Digital Dust Drawing Application
     </v-app-bar-title>
   </v-app-bar>
 </template>
